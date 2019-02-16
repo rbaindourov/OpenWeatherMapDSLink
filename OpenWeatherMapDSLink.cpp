095f4ec95b4d2da343f5d58da69c318db48c44fb
Robert Baindourov -X (rbaindou - BRILLIO LLC at Cisco) <rbaindou@cisco.com>
2019-02-16 14:30:33 -0800

    initiliazing project using Eclipse CDT

---
+++ OpenWeatherMapDSLink.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : OpenWeatherMapDSLink.cpp
+// Author      : Robert Baindourov
+// Version     :
+// Copyright   : GNU Public License
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("DSLink ");
+	return EXIT_SUCCESS;
+}
